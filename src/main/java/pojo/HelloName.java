2012f3a51fd3b6b3b18c8135ce5b49db52f89f54
Varun Kumar Reddy Gajjala <Varun@Varuns-MacBook-Pro.local>
2015-10-27 02:37:26 -0700

    Pushing helloname

---
+++ src/main/java/pojo/HelloName.java
@@ -0,0 +1,22 @@
+package pojo;
+
+public class HelloName {
+	private String name;
+	
+	public HelloName(){
+		
+	}
+	
+	public HelloName(String name){
+		this.name = name;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return this.name;
+	}
+
+}
