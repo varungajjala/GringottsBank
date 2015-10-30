5f299e8f0ae952be5ccb9c7e3cc1e5cf0037525c
Varun Kumar Reddy Gajjala <Varun@varuns-mbp.mobile.asu.edu>
2015-10-29 19:45:31 -0700

    Changed Controller Advice to controller

--- src/main/java/com/softwaresecurity/gringotts/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/softwaresecurity/gringotts/GlobalDefaultExceptionHandler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice
+@Controller
 class GlobalDefaultExceptionHandler {
     public static final String DEFAULT_ERROR_VIEW = "error";
 
